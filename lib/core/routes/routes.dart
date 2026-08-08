e58df5d821cd2b6123b7434ed58aff1280227051
Amr Reda <amr.reda2014@yahoo.com>
2026-08-09 02:38:16 +0300

    added route names and changed app Name

---
+++ lib/core/routes/routes.dart
@@ -0,0 +1,49 @@
+class Routes {
+
+  // Common
+  // =========================
+  static const String splashScreen = '/splashScreen';
+  static const String onboardingScreen = '/onboardingScreen';
+  static const String accountTypeScreen = '/accountTypeScreen';
+  static const String signInScreen = '/signInScreen';
+  static const String authenticationScreen = '/authenticationScreen';
+  static const String chatScreen = '/chatScreen';
+
+  // Customer
+  // =========================
+  static const String customerSignUpScreen = '/customerSignUpScreen';
+  static const String customerHomeScreen = '/customerHomeScreen';
+  static const String serviceInfoScreen = '/serviceInfoScreen';
+  static const String providerDetailsScreen = '/providerDetailsScreen';
+  static const String favoritesScreen = '/favoritesScreen';
+  static const String emergencyBookingScreen = '/emergencyBookingScreen';
+  static const String currentRequestScreen = '/currentRequestScreen';
+  static const String previousCompletedRequestScreen = '/previousCompletedRequestScreen';
+  static const String reviewsScreen = '/reviewsScreen';
+  static const String feedbackScreen = '/feedbackScreen';
+  static const String customerSupportScreen = '/customerSupportScreen';
+  static const String customerProfileScreen = '/customerProfileScreen';
+  static const String customerPersonalDataScreen ='/customerPersonalDataScreen';
+  static const String helpContactUsScreen = '/helpContactUsScreen';
+  static const String customerCommunityScreen = '/customerCommunityScreen';
+
+  // Service Provider
+  // =========================
+  static const String providerSignUpScreen = '/providerSignUpScreen';
+  static const String verificationStatusScreen = '/verificationStatusScreen';
+  static const String providerHomeScreen = '/providerHomeScreen';
+  static const String myServicesScreen = '/myServicesScreen';
+  static const String availableRequestsScreen = '/availableRequestsScreen';
+  static const String requestDetailsScreen = '/requestDetailsScreen';
+  static const String waitingForCustomerScreen = '/waitingForCustomerScreen';
+  static const String jobDetailsScreen = '/jobDetailsScreen';
+  static const String completionDetailsScreen = '/completionDetailsScreen';
+  static const String providerProfileScreen = '/providerProfileScreen';
+  static const String completedJobsScreen = '/completedJobsScreen';
+  static const String earningsScreen = '/earningsScreen';
+  static const String providerReviewsScreen = '/providerReviewsScreen';
+  static const String performanceMetricsScreen = '/performanceMetricsScreen';
+  static const String emergencyRequestsScreen = '/emergencyRequestsScreen';
+  static const String providerCommunityScreen = '/providerCommunityScreen';
+  static const String providerSupportScreen = '/providerSupportScreen';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
+import 'fix_now.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FixNow());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
-  }
-}
