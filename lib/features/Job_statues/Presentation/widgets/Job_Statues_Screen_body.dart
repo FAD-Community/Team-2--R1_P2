cde31e3e39e2c9c0c87fcee42c3fa67356cd274d
Ahmed Ehab <ahmedhobaa24@gmail.com>
2026-08-24 02:38:18 +0300

    finish job timeline ui

--- lib/features/Job_statues/Presentation/widgets/Job_Statues_Screen_body.dart
+++ lib/features/Job_statues/Presentation/widgets/Job_Statues_Screen_body.dart
@@ -1,5 +1,9 @@
+import 'package:fix_now_team_2/Features/Job_statues/Presentation/widgets/Job_Status_Timeline.dart';
 import 'package:fix_now_team_2/Features/Job_statues/Presentation/widgets/information_container.dart';
 import 'package:fix_now_team_2/Features/Job_statues/Presentation/widgets/service_provider_container.dart';
+import 'package:fix_now_team_2/Features/Job_statues/data/Models/Job_Status_Model.dart';
+import 'package:fix_now_team_2/core/theme/app_text_styles.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -9,10 +13,19 @@ class JobStatuesScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         InformationContainer(),
         SizedBox(height: 12.h),
         ServiceProviderContinar(),
+        SizedBox(height: 12.h),
+        Text(
+          'Job Status',
+          style: AppTextStyle.font24MediumPrimary.copyWith(color: Colors.black),
+        ),
+        SizedBox(height: 12.h),
+
+        JobStatusTimeline(currentStatus: JobStatus.inProgress),
       ],
     );
   }
