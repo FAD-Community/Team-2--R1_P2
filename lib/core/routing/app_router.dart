d8da72855d41b79fda2ddc2a0d5018a208ce1b56
Amr Reda <amr.reda2014@yahoo.com>
2026-08-26 01:23:30 +0300

    implementing provider sign up info one ui

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,11 +1,11 @@
 import 'package:fix_now_team_2/features/account_type/presentation/screens/account_type_screen.dart';
-import 'package:fix_now_team_2/features/auth/provider_auth/presentation/screens/provider_sign_up_info_one.dart';
 import 'package:fix_now_team_2/features/auth/provider_auth/presentation/screens/provider_sign_up_screen.dart';
 import 'package:fix_now_team_2/Features/Job_statues/Presentation/Screens/Job_Statues_Screen.dart';
 import 'package:fix_now_team_2/features/onboarding/presentation/screens/onboarding_screen_view.dart';
 import 'package:fix_now_team_2/features/splash/presentation/screens/splash_screen_view.dart';
 import 'package:flutter/material.dart';
 
+import '../../features/auth/provider_auth/presentation/screens/provider_sign_up_info_one_screen.dart';
 import 'routes.dart';
 
 class AppRouter {
@@ -22,8 +22,8 @@ class AppRouter {
       case Routes.providerSignUpScreen:
         return MaterialPageRoute(builder: (_) => const ProviderSignUpScreen());
 
-      case Routes.providerSignUpInfoOne:
-        return MaterialPageRoute(builder: (_) => const ProviderSignUpInfoOne());
+      case Routes.providerSignUpInfoOneScreen:
+        return MaterialPageRoute(builder: (_) => const ProviderSignUpInfoOneScreen());
       case Routes.jobDetailsScreen:
         return MaterialPageRoute(builder: (_) => const JobStatuesScreen());
 
