0148c0235610fbeab95d0790f7a336ec0fa9a321
Amr Reda <amr.reda2014@yahoo.com>
2026-08-31 16:12:43 +0300

    added custom text to core widget

--- lib/features/auth/provider_auth/presentation/screens/verification_status_screen.dart
+++ lib/features/auth/provider_auth/presentation/screens/verification_status_screen.dart
@@ -1,4 +1,6 @@
 import 'package:fix_now_team_2/core/theme/app_colors.dart';
+import 'package:fix_now_team_2/core/theme/app_text_styles.dart';
+import 'package:fix_now_team_2/core/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -15,9 +17,18 @@ class VerificationStatusScreen extends StatelessWidget {
             padding:  EdgeInsets.symmetric(horizontal: 19.w),
             child: Column(
               children: [
-               Image.asset("assets/images/provider/under_review_pic.png",
-               height: 352.h,
-               )
+               Image.asset(
+                 "assets/images/provider/under_review_pic.png",
+                  height: 352.h,
+               ),
+                SizedBox(
+                  height: 24.h,
+                ),
+                CustomText(
+                    text: "Provider Pending Approval",
+                    style: AppTextStyle.font28SemiBoldBlack
+                ),
+
               ],
             ),
           )
