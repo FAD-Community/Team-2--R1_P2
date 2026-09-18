55bf37829aeca6fe1e7f28a45220e6aae0ee3670
Amr Reda <amr.reda2014@yahoo.com>
2026-09-18 10:19:55 +0300

    added offers domain and data layer

--- lib/features/provider/data/models/offer_model.dart
+++ lib/features/provider/data/models/offer_model.dart
@@ -1,5 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../domain/entities/offer_entity.dart';
+
 part 'offer_model.freezed.dart';
 part 'offer_model.g.dart';
 
@@ -17,4 +19,17 @@ abstract class OfferModel with _$OfferModel {
 
   factory OfferModel.fromJson(Map<String, dynamic> json) =>
       _$OfferModelFromJson(json);
+}
+extension OfferModelMapper on OfferModel {
+  OfferEntity toEntity() {
+    return OfferEntity(
+      id: id,
+      requestId: requestId,
+      technicianId: technicianId,
+      proposedPrice: proposedPrice,
+      message: message,
+      status: status,
+      createdAt: createdAt,
+    );
+  }
 }
